c6d6c941e3fd22a215d25b929f75a613eabb5639
Andrei Labadaev <andrei-labadaev@mail.ru>
2024-04-12 20:37:29 +0300

    uppdate

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -117,197 +117,6 @@ maskPhone();
 
 
 
-
-function dynamic() {
-  document.addEventListener("DOMContentLoaded", function() {
-    function DynamicAdapt(type) {
-      this.type = type;
-    }
-    
-    DynamicAdapt.prototype.init = function () {
-      const _this = this;
-      // массив объектов
-      this.оbjects = [];
-      this.daClassname = "_dynamic_adapt_";
-      // массив DOM-элементов
-      this.nodes = document.querySelectorAll("[data-da]");
-    
-      // наполнение оbjects объктами
-      for (let i = 0; i < this.nodes.length; i++) {
-        const node = this.nodes[i];
-        const data = node.dataset.da.trim();
-        const dataArray = data.split(",");
-        const оbject = {};
-        оbject.element = node;
-        оbject.parent = node.parentNode;
-        оbject.destination = document.querySelector(dataArray[0].trim());
-        оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
-        оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
-        оbject.index = this.indexInParent(оbject.parent, оbject.element);
-        this.оbjects.push(оbject);
-      }
-    
-      this.arraySort(this.оbjects);
-    
-      // массив уникальных медиа-запросов
-      this.mediaQueries = Array.prototype.map.call(this.оbjects, function (item) {
-        return '(' + this.type + "-width: " + item.breakpoint + "px)," + item.breakpoint;
-      }, this);
-      this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
-        return Array.prototype.indexOf.call(self, item) === index;
-      });
-    
-      // навешивание слушателя на медиа-запрос
-      // и вызов обработчика при первом запуске
-      for (let i = 0; i < this.mediaQueries.length; i++) {
-        const media = this.mediaQueries[i];
-        const mediaSplit = String.prototype.split.call(media, ',');
-        const matchMedia = window.matchMedia(mediaSplit[0]);
-        const mediaBreakpoint = mediaSplit[1];
-    
-        // массив объектов с подходящим брейкпоинтом
-        const оbjectsFilter = Array.prototype.filter.call(this.оbjects, function (item) {
-          return item.breakpoint === mediaBreakpoint;
-        });
-        matchMedia.addListener(function () {
-          _this.mediaHandler(matchMedia, оbjectsFilter);
-        });
-        this.mediaHandler(matchMedia, оbjectsFilter);
-      }
-    };
-    
-    DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
-      if (matchMedia.matches) {
-        for (let i = 0; i < оbjects.length; i++) {
-          const оbject = оbjects[i];
-          оbject.index = this.indexInParent(оbject.parent, оbject.element);
-          this.moveTo(оbject.place, оbject.element, оbject.destination);
-        }
-      } else {
-        for (let i = 0; i < оbjects.length; i++) {
-          const оbject = оbjects[i];
-          if (оbject.element.classList.contains(this.daClassname)) {
-            this.moveBack(оbject.parent, оbject.element, оbject.index);
-          }
-        }
-      }
-    };
-    
-    // Функция перемещения
-    DynamicAdapt.prototype.moveTo = function (place, element, destination) {
-      element.classList.add(this.daClassname);
-      if (place === 'last' || place >= destination.children.length) {
-        destination.insertAdjacentElement('beforeend', element);
-        return;
-      }
-      if (place === 'first') {
-        destination.insertAdjacentElement('afterbegin', element);
-        return;
-      }
-      destination.children[place].insertAdjacentElement('beforebegin', element);
-    }
-    
-    // Функция возврата
-    DynamicAdapt.prototype.moveBack = function (parent, element, index) {
-      element.classList.remove(this.daClassname);
-      if (parent.children[index] !== undefined) {
-        parent.children[index].insertAdjacentElement('beforebegin', element);
-      } else {
-        parent.insertAdjacentElement('beforeend', element);
-      }
-    }
-    
-    // Функция получения индекса внутри родителя
-    DynamicAdapt.prototype.indexInParent = function (parent, element) {
-      const array = Array.prototype.slice.call(parent.children);
-      return Array.prototype.indexOf.call(array, element);
-    };
-    
-    // Функция сортировки массива по breakpoint и place 
-    // по возрастанию для this.type = min
-    // по убыванию для this.type = max
-    DynamicAdapt.prototype.arraySort = function (arr) {
-      if (this.type === "min") {
-        Array.prototype.sort.call(arr, function (a, b) {
-          if (a.breakpoint === b.breakpoint) {
-            if (a.place === b.place) {
-              return 0;
-            }
-    
-            if (a.place === "first" || b.place === "last") {
-              return -1;
-            }
-    
-            if (a.place === "last" || b.place === "first") {
-              return 1;
-            }
-    
-            return a.place - b.place;
-          }
-    
-          return a.breakpoint - b.breakpoint;
-        });
-      } else {
-        Array.prototype.sort.call(arr, function (a, b) {
-          if (a.breakpoint === b.breakpoint) {
-            if (a.place === b.place) {
-              return 0;
-            }
-    
-            if (a.place === "first" || b.place === "last") {
-              return 1;
-            }
-    
-            if (a.place === "last" || b.place === "first") {
-              return -1;
-            }
-    
-            return b.place - a.place;
-          }
-    
-          return b.breakpoint - a.breakpoint;
-        });
-        return;
-      }
-    };
-    
-    const da = new DynamicAdapt("max");
-    da.init();
-  })
-}
-dynamic()
-
-
-
-// аккордион
-const accordion = () => {
-	const btn = document.querySelectorAll(".accordion-btn");
-	if (btn) {
-		btn.forEach((item) => {
-			let accordionWrapp = item.closest(".accordion");
-			if (item.classList.contains("active")) {
-				accordionActive(accordionWrapp);
-			}
-			item.addEventListener("click", () => {
-				item.classList.toggle("active");
-				accordionActive(accordionWrapp);
-			});
-		});
-	}
-};
-accordion();
-
-function accordionActive(accordionWrapp) {
-	if (accordionWrapp) {
-		let panel = accordionWrapp.querySelector(".accordion__body");
-		if (panel.style.maxHeight) {
-			panel.style.maxHeight = null;
-		} else {
-			panel.style.maxHeight = panel.scrollHeight + "px";
-		}
-	}
-}
-
 const lightbox = GLightbox({
     touchNavigation: true,
     loop: true,
@@ -345,48 +154,58 @@ const menu = () => {
 };
 menu();
 
-const teamSwiper = () => {
-	const teamWrapper = document.querySelector(".team__swiper");
+const gallarySwiper = () => {
+	const gallaryWrapper = document.querySelector(".gallary__swiper");
 	const installSwiper = {
-		spaceBetween: 20,
-		slidesPerView: 3,
-		loop: true,
+		spaceBetween: 80,
+		slidesPerView: 1,
+		initialSlide: 1,
+		autoplay: {
+			delay: 4000,
+			reverseDirection: true,
+		},
+		pagination: {
+			el: ".gallary__swiper-pagination",
+			type: "bullets",
+		},
 		breakpoints: {
-			320: { slidesPerView: 1 },
-			450: { slidesPerView: 2 },
+			320: {
+				spaceBetween: 15,
+			},
+
 			772: {
-				slidesPerView: 3,
+				spaceBetween: 80,
 			},
 		},
-		navigation: {
-			nextEl: ".team__btn-next",
-			prevEl: ".team__btn-prev",
-		},
 	};
-	if (teamWrapper) {
-		const swiperTeam = new Swiper(teamWrapper, installSwiper);
+	if (gallaryWrapper) {
+		const swiperGallary = new Swiper(gallaryWrapper, installSwiper);
 	}
 };
-teamSwiper();
+gallarySwiper();
 
 const reviewsSwiper = () => {
 	const reviewsWrapper = document.querySelector(".reviews__swiper");
 	const installSwiper = {
-		spaceBetween: 40,
-		slidesPerView: 3,
-		watchSlidesProgress: true,
-		breakpoints: {
-			320: { slidesPerView: 1 },
-			772: { slidesPerView: 2 },
-			992: {
-				slidesPerView: 3,
-				spaceBetween: 20,
-			},
-			1400: { slidesPerView: 3, spaceBetween: 40 },
+		spaceBetween: 80,
+		slidesPerView: 1,
+		initialSlide: 1,
+		pagination: {
+			el: ".reviews__swiper-pagination",
+			type: "bullets",
 		},
 		navigation: {
 			nextEl: ".reviews__btn-next",
 			prevEl: ".reviews__btn-prev",
+		},
+		breakpoints: {
+			320: {
+				spaceBetween: 15,
+			},
+
+			772: {
+				spaceBetween: 80,
+			},
 		},
 	};
 	if (reviewsWrapper) {
@@ -395,6 +214,27 @@ const reviewsSwiper = () => {
 };
 
 reviewsSwiper();
+
+const reviewsSwiperText = () => {
+	const reviewsWrapperText = document.querySelector(".reviews__text-swiper");
+	const installSwiper = {
+		effect: "fade",
+		speed: 0,
+		allowTouchMove: false,
+		loop: true,
+		spaceBetween: 10,
+		slidesPerView: 1,
+		navigation: {
+			nextEl: ".reviews__text-btn-next",
+			prevEl: ".reviews__text-btn-prev",
+		},
+	};
+	if (reviewsWrapperText) {
+		const swiperReviewsText = new Swiper(reviewsWrapperText, installSwiper);
+	}
+};
+
+reviewsSwiperText();
 
 //fixed menu
 function fixedMenu() {

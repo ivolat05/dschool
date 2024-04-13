e32c848b11386f6425850cb43078ba05f110bfbb
Andrei Labadaev <andrei-labadaev@mail.ru>
2024-04-13 20:42:33 +0300

    add

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -372,7 +372,7 @@ const tab = () => {
 };
 tab();
 
-const dellBlock = () => {
+function dellBlock() {
 	const btnDell = document.querySelectorAll(".block--fixed-btn");
 	if (btnDell) {
 		btnDell.forEach((item) => {
@@ -384,6 +384,100 @@ const dellBlock = () => {
 			});
 		});
 	}
-};
+}
 dellBlock();
+
+const navLinkClassName = "scroll-btn";
+const navLinkActiveClassName = "--active";
+const sectionClassName = "section";
+const stopScroll = "stop-scroll";
+
+const navLinkNodes = document.querySelectorAll(`.${navLinkClassName}`);
+const navActiveLinkNode = document.querySelector(`.${navLinkActiveClassName}`);
+const sectionNodes = document.querySelectorAll(`.${sectionClassName}`);
+if (navLinkNodes) {
+	let indexActiveLink = 0;
+	let scorllAnimationId;
+	let currentScroll = window.scrollY;
+
+	changeNavActiveLink(navLinkNodes[indexActiveLink]);
+	navLinkNodes.forEach((btnClick) => {
+		btnClick.addEventListener("click", (evt) => {
+			evt.preventDefault();
+			const linkNavNode = evt.target.closest("a");
+
+			if (!linkNavNode) {
+				return;
+			}
+			if (linkNavNode.classList.contains(stopScroll)) {
+				return;
+			}
+
+			stopAnimationScroll();
+			changeNavActiveLink(linkNavNode);
+
+			currentScroll = window.scrollY;
+
+			const newSectionName = linkNavNode
+				.getAttribute("href")
+				.substring(1);
+			const newSectionElement = document.getElementById(newSectionName);
+			startAnimationScroll(newSectionElement.offsetTop);
+		});
+	});
+
+	window.addEventListener("scroll", () => {
+		if (!scorllAnimationId) {
+			setActiveLinkByScroll();
+		}
+	});
+
+	function startAnimationScroll(newScrollY) {
+		const deltaScroll = newScrollY - currentScroll;
+		let paddinTop = 40;
+		currentScroll += deltaScroll * 0.05;
+		window.scrollTo(0, currentScroll - paddinTop);
+
+		if (Math.abs(deltaScroll) > 1) {
+			scorllAnimationId = window.requestAnimationFrame(() => {
+				startAnimationScroll(newScrollY);
+			});
+		} else {
+			window.scrollTo(0, currentScroll - paddinTop);
+			stopAnimationScroll();
+		}
+	}
+
+	function stopAnimationScroll() {
+		window.cancelAnimationFrame(scorllAnimationId);
+		scorllAnimationId = undefined;
+	}
+
+	function changeNavActiveLink(newNavLinkNode) {
+		// для отоброжения на каком блоке сейчас находимся разкоментировать
+		// for (let i = 0; i < navLinkNodes.length; i++) {
+		// 	navLinkNodes[i].classList.remove(navLinkActiveClassName);
+		// }
+		// newNavLinkNode.classList.add(navLinkActiveClassName);
+	}
+
+	function setActiveLinkByScroll() {
+		const topSection = Array.from(sectionNodes).map(
+			(sectionNode) => sectionNode.offsetTop
+		);
+		let currentActiveIndex = 0;
+
+		for (let i = 0; i < topSection.length; i++) {
+			if (window.scrollY >= topSection[i]) {
+				currentActiveIndex = i;
+			}
+		}
+
+		if (indexActiveLink !== currentActiveIndex) {
+			indexActiveLink = currentActiveIndex;
+
+			changeNavActiveLink(navLinkNodes[indexActiveLink]);
+		}
+	}
+}
 
